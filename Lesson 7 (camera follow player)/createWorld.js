4022b7eff91712960fbea90fed2b59cbf755de0e
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2023-06-17 22:57:13 -0500

    keep player in frame

--- Lesson 7 (camera follow player)/createWorld.js
+++ Lesson 7 (camera follow player)/createWorld.js
@@ -1,12 +1,14 @@
 var worldObject = []
 
+var globalPos = [0, 0]
+
 var results;
 ImageParser("./map.png") 
 var check4results = setInterval(function(){
     if(results != undefined){
         clearInterval(check4results);
         for(var i = 0; i<results.length; i++){
-            for(var j = 0; j<results.length; j++){ 
+            for(var j = 0; j<results.length; j++){
                 if(results[i][j][0] == 0 && results[i][j][1] == 255 && results[i][j][2] == 255) {
                     worldObject.push(new Tree(i*tileSize, j*tileSize))
                 }
