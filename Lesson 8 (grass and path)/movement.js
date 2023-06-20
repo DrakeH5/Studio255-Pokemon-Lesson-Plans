aa7047e2cf229f7f6dc416b4962d4109c0accf9f
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2023-06-20 14:16:20 -0500

    grass and path

---
+++ Lesson 8 (grass and path)/movement.js
@@ -0,0 +1,64 @@
+function checkCollision(x1, y1, w1, h1, x2, y2, w2, h2){
+    return !(x1+w1 < x2 || x2+w2 < x1 
+    || y1+h1 < y2 || y2+h2 < y1)
+}
+
+
+document.addEventListener('keydown', function(e) {
+    if(e.keyCode == 87 || e.keyCode == 38){
+        for(var i=0; i<worldObject.length; i++){
+            if(checkCollision(player.x, player.y-2, 30, 30, worldObject[i].x+globalPos[1], worldObject[i].y+globalPos[0], worldObject[i].width, worldObject[i].height)){
+                return 
+            }
+        }
+        player.y-=2;
+        player.direction = 200;
+        player.momentum = -1;
+        if(player.y < 0){
+            globalPos[0]+=2
+            player.y+=2
+        }
+    }
+    if(e.keyCode == 83 || e.keyCode == 40){
+        for(var i=0; i<worldObject.length; i++){
+            if(checkCollision(player.x, player.y+2, 30, 30, worldObject[i].x+globalPos[1], worldObject[i].y+globalPos[0], worldObject[i].width, worldObject[i].height)){
+                return
+            }
+        }
+        player.y+=2;
+        player.direction = 10;
+        player.momentum = 1;
+        if(player.y+30 > canvas.height){
+            globalPos[0]-=2
+            player.y-=2
+        }
+    }
+    if(e.keyCode == 65 || e.keyCode == 37){
+        for(var i=0; i<worldObject.length; i++){
+            if(checkCollision(player.x-2, player.y, 30, 30, worldObject[i].x+globalPos[1], worldObject[i].y+globalPos[0], worldObject[i].width, worldObject[i].height)){
+                return
+            }
+        }
+        player.x-=2;
+        player.direction = 75;
+        player.momentum = -2;
+        if(player.x < 0){
+            globalPos[1]+=2
+            player.x+=2
+        }
+    }
+    if(e.keyCode == 68 || e.keyCode == 39){
+        for(var i=0; i<worldObject.length; i++){
+            if(checkCollision(player.x+2, player.y, 30, 30, worldObject[i].x+globalPos[1], worldObject[i].y+globalPos[0], worldObject[i].width, worldObject[i].height)){
+                return
+            }
+        }
+        player.x+=2;
+        player.direction = 140;
+        player.momentum = 2;
+        if(player.x+30 > canvas.width){
+            globalPos[1]-=2
+            player.x-=2
+        }
+    }
+})
