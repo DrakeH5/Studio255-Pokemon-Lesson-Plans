be4f8d7b0076d9b32ab78630f8378341a0d0e427
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2023-06-25 15:39:26 -0500

    tall grass

---
+++ Lesson 10 (high grass)/createWorld.js
@@ -0,0 +1,25 @@
+var worldObject = []
+var worldSprite = []
+
+var globalPos = [0, 0]
+
+var results;
+ImageParser("./map.png") 
+var check4results = setInterval(function(){
+    if(results != undefined){
+        clearInterval(check4results);
+        for(var i = 0; i<results.length; i++){
+            for(var j = 0; j<results.length; j++){
+                if(results[i][j][0] == 0 && results[i][j][1] == 255 && results[i][j][2] == 255) {
+                    worldObject.push(new Tree(i*tileSize, j*tileSize))
+                } else if(results[i][j][0] == 165 && results[i][j][1] == 121 && results[i][j][2] == 255) {
+                    worldSprite.push(new Path(i*tileSize, j*tileSize))
+                } else if(results[i][j][0] == 6 && results[i][j][1] == 109 && results[i][j][2] == 255) {
+                    worldSprite.push(new Grass(i*tileSize, j*tileSize))
+                } else if(results[i][j][0] == 164 && results[i][j][1] == 246 && results[i][j][2] == 255) {
+                    worldSprite.push(new HighGrass(i*tileSize, j*tileSize))
+                }
+            }
+        }
+    }
+}, 100)
